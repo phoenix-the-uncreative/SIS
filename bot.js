9c1759868300bd30fc819d36b880b0c873933785
TheEmperorPhoenix <thephoenixempirehq@gmail.com>
2021-01-22 23:47:07 +0300

    indents & cosmetic shit

--- bot.js
+++ bot.js
@@ -3,61 +3,76 @@ const fs = require('fs');
 const database = require("./database.js");
 const bot = new Discord.Client();
 
-let anonCooldowned = [], anonChannel = [], anonSend = [];
+let anonCooldowned = [],
+  anonChannel = [],
+  anonSend = [];
 
 let config = JSON.parse(fs.readFileSync('config.json', (err, data) => {
-	config = data;
+  config = data;
 }));
 
 bot.on('ready', () => {
-	console.log(`Logged in as ${bot.user.tag}! || ${new Date}`);
+  console.log(`Logged in as ${bot.user.tag}! || ${new Date}`);
 });
 
 bot.on('message', msg => {
-	let text = msg.content.toLowerCase();
-	let sisChannels = bot.guilds.cache.get("801494125064355957").channels.cache;
-	
-	if (msg.author.id == bot.user.id) return;
-	
-	database.connection.query(`SELECT * FROM \`${config.table}\` WHERE \`${config.column}\` = ${msg.author.id} and \`${config.column_ban}\` = 0`)
-		.on('result', result => {
-				if (result.length == 0) {
-					msg.channel.send(
-						"За вашим аккаунтом Discord не закреплено ни одного персонажа Морганы или все ваши персонажи забанены. Публиковать анонимные объявления без персонажей запрещено в целях безопасности."
-					);
-				} else if (msg.channel.type == "dm") {
-					if (anonCooldowned[msg.channel.id] == (false || undefined)) {
-						if (text == "анон старт") {
-							msg.channel.send(
-								"**ШИС: Отправка анонимных сообщений**\nВведите название канала, куда это сообщение будет отправлено (например: \"куплю\", \"разные-объявления\"):"
-							);
-							anonSend[msg.channel.id] = 1;
-						} else if (anonSend[msg.channel.id] == 1) {
-							if ((sisChannels.find(channel => channel.name === msg.content) != undefined) && 
-							(sisChannels.find(channel => channel.name === msg.content).parentID == "801502233845563462")) {
-								anonChannel[msg.channel.id] = sisChannels.find(channel => channel.name === msg.content).id;
-								anonSend[msg.channel.id] = 2;
-								msg.channel.send("Канал #" + msg.content +
-									" успешно выбран. Введите сообщение, которое будет туда анонимно отправлено:"
-									);
-							} else msg.channel.send(
-								"Канал не найден или не находится в категории объявлений. Попробуйте снова."
-								);
-						} else if (anonSend[msg.channel.id] == 2) {
-							sisChannels.get(anonChannel[msg.channel.id])
-							.send(`**Анонимное объявление:**${msg.content}`);
-							sisChannels.get("801787710410588170")
-							.send(`- Принято анонимное объявление от пользователя ${msg.author.tag} за ${`${new Date().toLocaleString()}.\n\n**Текст объявления**:\n`}${msg.content}`);
-							msg.channel
-							.send("Сообщение принято и отправлено. Следующее анонимное сообщение может быть отправлено только через час.\nСпасибо за использование услуги анонимной отправки сообщений.");
-							anonCooldowned[msg.channel.id] = true;
-							anonSend[msg.channel.id] = 0;
-							setTimeout(() => anonCooldowned[msg.channel.id] = false, 3600000);
-						} else msg.channel.send("Я реагирую только на команду \"анон старт\", а на другие темы разговаривать не умею. Пока.");
-					} else msg.channel.send("Возможна отправка лишь одного анонимного сообщения в час. Пожалуйста, подождите.");
-				};
-			})
-		.on('error', err => callback({ error: true, err: err }));
+  let text = msg.content.toLowerCase();
+  let sisChannels = bot.guilds.cache.get("801494125064355957").channels.cache;
+  
+  if (msg.author.id == bot.user.id) return;
+  
+  database.connection.query(
+      `SELECT * FROM \`${config.table}\` WHERE \`${config.column}\` = ${msg.author.id} and \`${config.column_ban}\` = 0`
+    )
+    .on('result', result => {
+      if (result.length == 0) {
+        msg.channel.send(
+          "За вашим аккаунтом Discord не закреплено ни одного персонажа Морганы или все ваши персонажи забанены. Публиковать анонимные объявления без персонажей запрещено в целях безопасности."
+        );
+      } else if (msg.channel.type == "dm") {
+        if (anonCooldowned[msg.channel.id] == (false || undefined)) {
+          if (text == "анон старт") {
+            msg.channel.send(
+              "**ШИС: Отправка анонимных сообщений**\nВведите название канала, куда это сообщение будет отправлено (например: \"куплю\", \"разные-объявления\"):"
+            );
+            
+            anonSend[msg.channel.id] = 1;
+          } else if (anonSend[msg.channel.id] == 1) {
+			if ((sisChannels.find(channel => channel.name === msg.content) != undefined) 
+			&& (sisChannels.find(channel => channel.name === msg.content).parentID == "801502233845563462")) {
+              anonChannel[msg.channel.id] = sisChannels.find(channel => channel.name === msg.content).id;
+              anonSend[msg.channel.id] = 2;
+              
+              msg.channel.send(
+                `Канал #${msg.content} успешно выбран. Введите сообщение, которое будет туда анонимно отправлено:`
+              );
+            } else msg.channel.send("Канал не найден или не находится в категории объявлений. Попробуйте снова.");
+          } else if (anonSend[msg.channel.id] == 2) {
+            sisChannels.get(anonChannel[msg.channel.id])
+              .send(`**Анонимное объявление:**${msg.content}`);
+            sisChannels.get("801787710410588170")
+              .send(
+				`- Принято анонимное объявление от пользователя ${msg.author.tag} за ${`${new Date().toLocaleString()}.
+				\n\n**Текст объявления**:\n`}${msg.content}`
+              );
+            msg.channel
+              .send(
+				`Сообщение принято и отправлено. Следующее анонимное сообщение может быть отправлено только через час.
+				\nСпасибо за использование услуги анонимной отправки сообщений.`
+              );
+            anonCooldowned[msg.channel.id] = true;
+            anonSend[msg.channel.id] = 0;
+            setTimeout(() => anonCooldowned[msg.channel.id] = false, 3600000);
+          } else msg.channel.send(
+            "Я реагирую только на команду \"анон старт\", а на другие темы разговаривать не умею. Пока.");
+        } else msg.channel.send(
+          "Возможна отправка лишь одного анонимного сообщения в час. Пожалуйста, подождите.");
+      };
+    })
+    .on('error', err => callback({
+      error: true,
+      err: err
+    }));
 });
 
 bot.login(config.token);
--- database.js
+++ database.js
@@ -2,40 +2,40 @@ const mysql = require('mysql');
 const fs = require('fs');
 
 let config = JSON.parse(fs.readFileSync('config.json', (err, data) => {
-	config = data;
+  config = data;
 }));
 
 let sqlPool = mysql.createPool({
-	host: config.host,
-	user: config.user,
-	password: config.password,
-	database: config.database
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
 });
 
 exports.connection = {
-    query: function() {
-		var queryArgs = Array.prototype.slice.call(arguments),
-			events = [],
-			eventNameIndex = {};
-
-		sqlPool.getConnection(function (err, conn) {
-			if (err)
-				if (eventNameIndex.error)
-					eventNameIndex.error();
-			if (conn) {
-				var q = conn.query.apply(conn, queryArgs);
-				q.on('end', () => conn.release());
-
-				events.forEach(args => q.on.apply(q, args));
-			}
-		});
-
-		return {
-			on: function (eventName, callback) {
-				events.push(Array.prototype.slice.call(arguments));
-				eventNameIndex[eventName] = callback;
-				return this;
-			}
-		};
-	}
+  query: function() {
+    var queryArgs = Array.prototype.slice.call(arguments),
+      events = [],
+      eventNameIndex = {};
+    
+    sqlPool.getConnection(function(err, conn) {
+      if (err)
+        if (eventNameIndex.error)
+          eventNameIndex.error();
+      if (conn) {
+        var q = conn.query.apply(conn, queryArgs);
+        q.on('end', () => conn.release());
+        
+        events.forEach(args => q.on.apply(q, args));
+      }
+    });
+    
+    return {
+      on: function(eventName, callback) {
+        events.push(Array.prototype.slice.call(arguments));
+        eventNameIndex[eventName] = callback;
+        return this;
+      }
+    };
+  }
 };
